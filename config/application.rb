0d6348c93761996aff839a3e68f9033c5844c660
Manoj Naidu <manojnaidu619@gmail.com>
2019-07-22 14:21:09 +0530

    Added rack-cors

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,12 @@ module RailsAuthBackend
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:8080'
+        resource '*', headers: :any, methods: [:get, :post]
+      end
+    end
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
